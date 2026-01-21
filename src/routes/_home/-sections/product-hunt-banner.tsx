b3c342b0292b4f3f0c0c8390268279aa77d53e9f
Amruth Pillai <im.amruth@gmail.com>
2026-01-21 05:51:13 +0100

    change launch date

--- src/routes/_home/-sections/product-hunt-banner.tsx
+++ src/routes/_home/-sections/product-hunt-banner.tsx
@@ -2,8 +2,8 @@ import { Trans } from "@lingui/react/macro";
 import { HeartIcon } from "@phosphor-icons/react";
 import { useEffect, useState } from "react";
 
-const PH_LAUNCH_START = Date.UTC(2026, 0, 21, 8, 1, 0);
-const PH_LAUNCH_END = Date.UTC(2026, 0, 22, 8, 1, 0);
+const PH_LAUNCH_START = Date.UTC(2026, 1, 1, 8, 1, 0);
+const PH_LAUNCH_END = Date.UTC(2026, 1, 2, 8, 1, 0);
 
 function isWithinProductHuntLaunchWindow() {
 	const nowUtc = Date.now();
